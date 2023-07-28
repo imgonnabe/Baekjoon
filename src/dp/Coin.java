14a177f48a7704d0cfcb50bd10a5c4f82223e1fa
imgonnabe <qorlwn456123@gmail.com>
2023-07-28 17:47:55 +0900

    동전

---
+++ src/dp/Coin.java
@@ -0,0 +1,20 @@
+package dp;
+
+import java.util.Scanner;
+
+public class Coin {
+	static Integer[] dp;
+	
+	static int coin(int n, int k, int i) {
+		if(dp[i] == null) {
+			
+		}
+		return dp[n];
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] arr = str.split(" ");
+	}
+}
